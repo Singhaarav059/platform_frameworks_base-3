f3f5be3d3e9bb732604f44592292221e4ddbd329
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 01:56:16 +0000

    Configurable 0, 90, 180 and 270 degree rotation
    Change-Id: Ia1859c51d71ef9d01cec2d13c6468ed89c6ac53e
    Contributor: jonasl
    
    - Add system setting
    
    Author: Tim Schumacher <timschumi@gmx.de>
    Date:   Wed Nov 28 21:44:18 2018 +0100
    
        Add back ACCELEROMETER_ROTATION_ANGLES and update references
    
        This needed to move back into fw/b, because it needs to be
        accessed from inside the RotationPolicy. Previously
        (when this constant and Setting were located in lineage-sdk),
        the settings for the display rotation had no effect, because we
        couldn't query the correct set of settings.
    
        Change-Id: Icce249925a578c328db3884e5d332b20d6e7db6c
        Fixes: BUGBASH-2042
    
    Author: eray orçunus <erayorcunus@gmail.com>
    Date:   Mon Jun 22 22:47:40 2015 +0300
    
        Rotation related corrections
    
        - There are some conditions deleted while placing rotation angles code, I added them.
    
        - Rotation lock was screwed up since CM 12. Fixed it by fetching allowed rotations from CM's
        allowed rotations setting.
    
        - Also, a CAF commit had killed rotation lock ability.
    
        [port to 15.1]:
         - ACCELEROMETER_ROTATION_ANGLES moved to LineageSDK
         - Slight change of the WindowManager API
    
        [port to 16.0]:
         - adjust context
         - ACCELEROMETER_ROTATION_ANGLES moved to Settings
         - Use the configstore API
    
        Change-Id: I8f1b468249c68e7b6514d1a96bdb3fc638af84fd
        Signed-off-by: eray orçunus <erayorcunus@gmail.com>
        (cherry picked from commit a62720d51e9d8f3a3c9992ea0bdb707b64e865b5)
    
    Author: Tim Schumacher <timschumi@gmx.de>
    Date:   Thu May 2 19:48:39 2019 +0200
    
        RotationPolicy: Don't crash if configstore 1.1 isn't available
    
        Change-Id: I77301ec8c72393daa0003ca310eee07b767d4e69
    
    Change-Id: Ia7bf8cb64258e1d602230a8f9ea227d3b56a4dab

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4087,6 +4087,23 @@ public final class Settings {
         public static final String ANIMATOR_DURATION_SCALE = Global.ANIMATOR_DURATION_SCALE;
 
         /**
+         * Control the type of rotation which can be performed using the accelerometer
+         * if ACCELEROMETER_ROTATION is enabled.
+         * Value is a bitwise combination of
+         * 1 = 0 degrees (portrait)
+         * 2 = 90 degrees (left)
+         * 4 = 180 degrees (inverted portrait)
+         * 8 = 270 degrees (right)
+         * Setting to 0 is effectively orientation lock
+         * @hide
+         */
+        public static final String ACCELEROMETER_ROTATION_ANGLES = "accelerometer_rotation_angles";
+
+        /** @hide */
+        public static final Validator ACCELEROMETER_ROTATION_ANGLES_VALIDATOR =
+                NON_NEGATIVE_INTEGER_VALIDATOR;
+
+        /**
          * Control whether the accelerometer will be used to change screen
          * orientation.  If 0, it will not be used unless explicitly requested
          * by the application; if 1, it will be used by default unless explicitly
@@ -5470,6 +5487,8 @@ public final class Settings {
             VALIDATORS.put(STATUSBAR_CLOCK_DATE_STYLE, STATUSBAR_CLOCK_DATE_STYLE_VALIDATOR);
             VALIDATORS.put(STATUSBAR_CLOCK_DATE_FORMAT, STATUSBAR_CLOCK_DATE_FORMAT_VALIDATOR);
             VALIDATORS.put(STATUSBAR_CLOCK_DATE_POSITION, STATUSBAR_CLOCK_DATE_POSITION_VALIDATOR);
+            VALIDATORS.put(ACCELEROMETER_ROTATION_ANGLES,
+                    ACCELEROMETER_ROTATION_ANGLES_VALIDATOR);
         }
 
         /**
