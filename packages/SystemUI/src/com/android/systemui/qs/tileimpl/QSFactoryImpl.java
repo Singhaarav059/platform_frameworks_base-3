be331960754e92da49f73464f1fcce02fb242eff
STELIX <ssspinni@gmail.com>
2015-12-20 19:27:06 +0100

    Add SleepScreen QS Tile (PowerMenu on longpress)

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -58,6 +58,7 @@ import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.SleepScreenTile;
 import com.android.systemui.qs.tiles.ScreenrecordTile;
 import com.android.systemui.qs.tiles.SoundTile;
 import com.android.systemui.qs.tiles.ScreenshotTile;
@@ -174,6 +175,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new CameraTile(mHost);
             case "call":
                 return new CallTile(mHost);
+            case "sleepscreen":
+                return new SleepScreenTile(mHost);
         }
 
         // Intent tiles.
