a04df293e0edd8e373b04910a82f5a896533b91b
Danny Baumann <dannybaumann@web.de>
2016-01-02 19:24:50 -0500

    base: Add back increasing ring feature (1/3)
    Change-Id: Id8344bd8a2e9e5321b9801eb081e0ab299467794
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5008,6 +5008,33 @@ public final class Settings {
          */
         public static final String QS_HEADER_STYLE = "qs_header_style";
 
+	/**
+         * Whether the phone ringtone should be played in an increasing manner
+         * @hide
+         */
+        public static final String INCREASING_RING = "increasing_ring";
+         /** @hide */
+        public static final Validator INCREASING_RING_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Start volume fraction for increasing ring volume
+         * @hide
+         */
+        public static final String INCREASING_RING_START_VOLUME = "increasing_ring_start_vol";
+         /** @hide */
+        public static final Validator INCREASING_RING_START_VOLUME_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
+        /**
+         * Ramp up time (seconds) for increasing ring
+         * @hide
+         */
+        public static final String INCREASING_RING_RAMP_UP_TIME = "increasing_ring_ramp_up_time";
+         /** @hide */
+        public static final Validator INCREASING_RING_RAMP_UP_TIME_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
@@ -5083,6 +5110,9 @@ public final class Settings {
             STATUS_BAR_CLOCK_SHOW_AM_PM,
             STATUS_BAR_CLOCK_SHOW_DAY,
             NIGHT_BRIGHTNESS_VALUE,
+            INCREASING_RING,
+            INCREASING_RING_START_VOLUME,
+            INCREASING_RING_RAMP_UP_TIME
         };
 
         /**
@@ -5216,6 +5246,9 @@ public final class Settings {
             PRIVATE_SETTINGS.add(SYSTEM_THEME_STYLE);
             PRIVATE_SETTINGS.add(NIGHT_BRIGHTNESS_VALUE);
             PRIVATE_SETTINGS.add(QS_HEADER_STYLE);
+            PRIVATE_SETTINGS.add(INCREASING_RING);
+            PRIVATE_SETTINGS.add(INCREASING_RING_START_VOLUME);
+            PRIVATE_SETTINGS.add(INCREASING_RING_RAMP_UP_TIME);
         }
 
 
@@ -5331,6 +5364,9 @@ public final class Settings {
             VALIDATORS.put(STATUS_BAR_CLOCK_SHOW_AM_PM, STATUS_BAR_CLOCK_SHOW_AM_PM_VALIDATOR);
             VALIDATORS.put(STATUS_BAR_CLOCK_SHOW_DAY, STATUS_BAR_CLOCK_SHOW_DAY_VALIDATOR);
             VALIDATORS.put(NIGHT_BRIGHTNESS_VALUE, NIGHT_BRIGHTNESS_VALUE_VALIDATOR);
+            VALIDATORS.put(INCREASING_RING, INCREASING_RING_VALIDATOR);
+            VALIDATORS.put(INCREASING_RING_START_VOLUME, INCREASING_RING_START_VOLUME_VALIDATOR);
+            VALIDATORS.put(INCREASING_RING_RAMP_UP_TIME, INCREASING_RING_RAMP_UP_TIME_VALIDATOR);
         }
 
         /**
