0b6bd5050d4a2d514fac5007b287a78c43ed6749
Nico60 <ndhouailly@gmail.com>
2016-05-29 13:05:37 +0200

    Add interpolators to qs tiles animation [1/2]
    Change-Id: Iaa1923e378e54ea452a2006dcade2bb1cd049d21
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6510,6 +6510,13 @@ public final class Settings {
         private static final Validator ANIM_TILE_DURATION_VALIDATOR =
               new SettingsValidators.InclusiveIntegerRangeValidator(1500, 2500);
 
+	/**
+         * Change quick settings tiles interpolator
+         *
+         * @hide
+         */
+        public static final String ANIM_TILE_INTERPOLATOR = "anim_tile_interpolator";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
