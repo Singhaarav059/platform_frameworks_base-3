d102cb8db9512b95416fff68e3ca41ec4c1e6eb2
ezio84 <brabus84@gmail.com>
2017-09-06 23:59:02 +0200

    Allow to disable qs footer warnings [1/2]
    When using apps like AdGuard that create a vpn, a warning is displayed
    in the QS panel footer, so the user knows the device net is being monitored
    by an external app.
    The warning takes space and the user might want to avoid it if he already
    knows that the app is running (also because there's a statusbar icon
    for the same purpose)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4676,6 +4676,9 @@ public class StatusBar extends SystemUI implements DemoMode,
                     Settings.System.HEADS_UP_STOPLIST_VALUES), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HEADS_UP_BLACKLIST_VALUES), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_FOOTER_WARNINGS),
+                    false, this, UserHandle.USER_ALL);
         }
          @Override
         public void onChange(boolean selfChange, Uri uri) {
@@ -4684,6 +4687,7 @@ public class StatusBar extends SystemUI implements DemoMode,
          public void update() {
             setHeadsUpStoplist();
             setHeadsUpBlacklist();
+            setQsPanelOptions();
         }
     }
 
@@ -4696,6 +4700,12 @@ public class StatusBar extends SystemUI implements DemoMode,
         final String blackString = Settings.System.getString(mContext.getContentResolver(),
                     Settings.System.HEADS_UP_BLACKLIST_VALUES);
         splitAndAddToArrayList(mBlacklist, blackString, "\\|");
+    }
+
+    private void setQsPanelOptions() {
+        if (mQSPanel != null) {
+            mQSPanel.updateSettings();
+        }
     }
 
     public int getWakefulnessState() {
