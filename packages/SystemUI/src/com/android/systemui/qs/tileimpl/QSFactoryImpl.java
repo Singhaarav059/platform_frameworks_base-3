fe093bf12a0c3e167522d04ba561d6a597d467e1
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    Introduce Theme tile
    Click to show detail view.
    
    Extract & Aapted for ColtOS from -
    
    https://github.com/DirtyUnicorns/android_frameworks_base/commit/205301221b06136b981f13304d9c35a07f3f2c0b#diff-a2f945e2d58b7e8c989d272123c63f22
    
    Change-Id: Ifa1e1cb6f92afbfa2fd2839b1c7586a06be5c339
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -54,6 +54,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RebootTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.SyncTile;
@@ -290,6 +291,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mFPSInfoTileProvider.get();
             case "sync":
                 return mSyncTileProvider.get();
+            case "theme":
+                return new ThemeTile(mHost);
         }
 
         // Intent tiles.
