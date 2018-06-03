7f6359b979d76dca6076702a506d34f368121954
ezio84 <brabus84@gmail.com>
2018-06-04 00:36:00 +0200

    Ambient music ticker AoD: make it compatibile with double tap sensor
    also don't wake up on skip track events when pulsing Ambient
    
    TLDR: some devices like the Pixel2xl have a specific double tap sensor for AoD
    that handles the touches in this screen state and nothing is dispatched
    to StatusBarWindowView, and it's enabled by the double tap to wake
    option available in Settings - System - Gestures.
    
    We can use the related onDoubleTap callback that Google already added
    on purpose to manage double taps  on the AmbientContainer (Now Playing on
    Android P) when AoD is showing, to trigger the skip track event we need.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Adhitya Mohan <me@adhityamohan.in>

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -220,6 +220,10 @@ public class KeyguardStatusView extends GridLayout implements
         updateSettings();
     }
 
+    public KeyguardSliceView getKeyguardSliceView() {
+        return mKeyguardSlice;
+    }
+
     /**
      * Moves clock, adjusting margins when slice content changes.
      */
