8aaee48d3687f6aa117582aba92da9bcbf81bb61
faust93 <monumentum@gmail.com>
2018-09-09 21:10:06 +0800

    AlarmsBlocker [1/2]
    Made by @faust93. Based on WakelockBlocker by @maxwen

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4830,6 +4830,18 @@ public final class Settings {
         public static final String SYSTEM_THEME_STYLE = "system_theme_style";
 
         /**
+         * List of alarms blocks selected
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_LIST = "alarm_blocking_list";
+
+        /**
+         * Enable blocking alarm
+         * @hide
+         */
+        public static final String ALARM_BLOCKING_ENABLED = "alarm_blocking_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
