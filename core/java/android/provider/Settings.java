c3c5df5917f7083becbca3b9dece691da5f1938c
Shubham Singh <coolsks94@gmail.com>
2018-10-08 17:09:12 +0530

    fwb: Allow users to switch between Pie/Oreo recents [1/3]
    I personally don't like Pie's recent style built in launcher3,
    Hence allow to switch between recents components(Quickstep/SystemUI) without much hassle.
    Other two parts of commit in Extensions and Settings will also take care of swipe up gesture.
    If it is enabled it will be disabled and removed from Settings>System>Gestures, if in case Oreo style recents is selected by user.
    
    Commit's logic is based on https://github.com/PotatoProject/frameworks_base/commit/74f8b464a944cff9f382148521aab52d0c054136 , so huge thanks to @deletescape
    
    Change-Id: I2c0657bb798c515bd946bf1885d355520a8b98c0
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4528,6 +4528,18 @@ public final class Settings {
         public static final String SHOW_FOURG_ICON = "show_fourg_icon";
 
         /**
+         * Which component to use for Recents UI
+         * 0 - Pie Recents (Quickstep)
+         * 1 - Oreo Recents (SystemUI)
+         * @hide
+         */
+        public static final String RECENTS_COMPONENT = "recents_component";
+
+        /** @hide */
+        public static final Validator RECENTS_COMPONENT_VALIDATOR =
+                new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4594,6 +4606,7 @@ public final class Settings {
             OMNI_QS_QUICKBAR_COLUMNS,
             OMNI_QS_PANEL_BG_ALPHA,
             OMNI_USE_BOTTOM_GESTURE_NAVIGATION,
+            RECENTS_COMPONENT,
         };
 
         /**
@@ -4717,6 +4730,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(OMNI_QS_PANEL_BG_ALPHA);
             PRIVATE_SETTINGS.add(OMNI_USE_BOTTOM_GESTURE_NAVIGATION);
             PRIVATE_SETTINGS.add(QS_TILE_STYLE);       
+            PRIVATE_SETTINGS.add(RECENTS_COMPONENT);
         }
 
 
@@ -4823,6 +4837,7 @@ public final class Settings {
             VALIDATORS.put(OMNI_QS_PANEL_BG_ALPHA, OMNI_QS_PANEL_BG_ALPHA_VALIDATOR);
             VALIDATORS.put(OMNI_USE_BOTTOM_GESTURE_NAVIGATION,
                     OMNI_USE_BOTTOM_GESTURE_NAVIGATION_VALIDATOR);
+            VALIDATORS.put(RECENTS_COMPONENT,RECENTS_COMPONENT_VALIDATOR);
         }
 
         /**
