44072fa811992a5c1da90b4193fdab84b5bca406
Anushek Prasal <anushekprasal@gmail.com>
2018-11-12 01:10:05 +0530

    Add QS header styles [1/3]
    Gives the user a choice between black, dark grey, light grey and accent based options.
    
    Based on the code of Fruity pebbles and QS tile picker.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5004,6 +5004,11 @@ public final class Settings {
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
         /**
+         * @hide
+         */
+        public static final String QS_HEADER_STYLE = "qs_header_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5210,6 +5215,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(STATUS_BAR_CLOCK_SHOW_DAY);
             PRIVATE_SETTINGS.add(SYSTEM_THEME_STYLE);
             PRIVATE_SETTINGS.add(NIGHT_BRIGHTNESS_VALUE);
+            PRIVATE_SETTINGS.add(QS_HEADER_STYLE);
         }
 
 
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -351,6 +351,15 @@ public class StatusBar extends SystemUI implements DemoMode,
         "com.android.systemui.qstile.oreosquircletrim", // 14
     };
 
+    // QS header themes
+    private static final String[] QS_HEADER_THEMES = {
+        "com.android.systemui.qsheader.black", // 0
+        "com.android.systemui.qsheader.grey", // 1
+        "com.android.systemui.qsheader.lightgrey", // 2
+        "com.android.systemui.qsheader.accent", // 3
+        "com.android.systemui.qsheader.transparent", // 4
+    };
+
     /** If true, the system is in the half-boot-to-decryption-screen state.
      * Prudently disable QS and notifications.  */
     private static final boolean ONLY_CORE_APPS;
@@ -4111,6 +4120,18 @@ public class StatusBar extends SystemUI implements DemoMode,
         stockNewTileStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
     }
 
+    // Switches qs header style from stock to custom
+    public void updateQSHeaderStyle() {
+        int qsHeaderStyle = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.QS_HEADER_STYLE, 0, mLockscreenUserManager.getCurrentUserId());
+        updateNewQSHeaderStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId(), qsHeaderStyle);
+    }
+
+    // Unload all qs header styles back to stock
+    public void stockQSHeaderStyle() {
+        stockNewQSHeaderStyle(mOverlayManager, mLockscreenUserManager.getCurrentUserId());
+    }
+
     private void updateDozingState() {
         Trace.traceCounter(Trace.TRACE_TAG_APP, "dozing", mDozing ? 1 : 0);
         Trace.beginSection("StatusBar#updateDozingState");
@@ -4787,7 +4808,10 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                       Settings.System.QS_TILE_STYLE),
                     false, this, UserHandle.USER_ALL);
-	 }
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.QS_HEADER_STYLE),
+                    false, this, UserHandle.USER_ALL);
+        }
 
          @Override
          public void onChange(boolean selfChange, Uri uri) {
@@ -4795,9 +4819,13 @@ public class StatusBar extends SystemUI implements DemoMode,
 	         Settings.System.QS_TILE_STYLE))) {
 	         stockTileStyle();
 	         updateTileStyle();
-	    }
-	  update();
-         }
+            } else if (uri.equals(Settings.System.getUriFor(
+                    Settings.System.QS_HEADER_STYLE))) {
+                stockQSHeaderStyle();
+                updateQSHeaderStyle();
+            }
+	    update();
+        }
 
          public void update() {
             setHeadsUpStoplist();
@@ -4864,6 +4892,34 @@ public class StatusBar extends SystemUI implements DemoMode,
             String qstiletheme = QS_TILE_THEMES[i];
             try {
                 om.setEnabled(qstiletheme,
+                        false /*disable*/, userId);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Switches qs header style to user selected.
+    public static void updateNewQSHeaderStyle(IOverlayManager om, int userId, int qsHeaderStyle) {
+        if (qsHeaderStyle == 0) {
+            stockNewQSHeaderStyle(om, userId);
+        } else {
+            try {
+                om.setEnabled(QS_HEADER_THEMES[qsHeaderStyle],
+                        true, userId);
+            } catch (RemoteException e) {
+                Log.w(TAG, "Can't change qs header theme", e);
+            }
+        }
+    }
+
+    // Switches qs header style back to stock.
+    public static void stockNewQSHeaderStyle(IOverlayManager om, int userId) {
+        // skip index 0
+        for (int i = 1; i < QS_HEADER_THEMES.length; i++) {
+            String qsheadertheme = QS_HEADER_THEMES[i];
+            try {
+                om.setEnabled(qsheadertheme,
                         false /*disable*/, userId);
             } catch (RemoteException e) {
                 e.printStackTrace();
