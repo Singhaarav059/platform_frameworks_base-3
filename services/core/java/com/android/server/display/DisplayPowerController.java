omnirom/android_frameworks_base_old

base: fix defauilt value for screen off animation setting
Change-Id: If1b12ee466c26eb6f272bb66ff2b594236e0d216

services/core/java/com/android/server/display/DisplayPowerController.java
@@ -567,7 +567,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             @Override
             public void onChange(boolean selfChange, Uri uri) {
                 mScreenOffAnimation = Settings.Global.getInt(cr,
-                        Settings.Global.SCREEN_OFF_ANIMATION, 1);
+                        Settings.Global.SCREEN_OFF_ANIMATION, SCREEN_OFF_SIMPLE_FADE);
                 mScreenOnAnimation = Settings.Global.getInt(cr,
                         Settings.Global.SCREEN_ON_ANIMATION, 0) != 0;
                 if (mPowerState != null) {
@@ -582,7 +582,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 Settings.Global.SCREEN_ON_ANIMATION),
                 false, observer, UserHandle.USER_ALL);
         mScreenOffAnimation = Settings.Global.getInt(cr,
-                Settings.Global.SCREEN_OFF_ANIMATION, 1);
+                Settings.Global.SCREEN_OFF_ANIMATION, SCREEN_OFF_SIMPLE_FADE);
         mScreenOnAnimation = Settings.Global.getInt(cr,
                 Settings.Global.SCREEN_ON_ANIMATION, 0) != 0;
 

Singhaarav059/platform_frameworks_base-3

Signed-off-by: mydongistiny <jaysonedson@gmail.com>

services/core/java/com/android/server/display/DisplayPowerController.java
@@ -643,7 +643,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
             @Override
             public void onChange(boolean selfChange, Uri uri) {
                 mScreenOffAnimation = Settings.Global.getInt(cr,
-                        Settings.Global.SCREEN_OFF_ANIMATION, 1);
+                        Settings.Global.SCREEN_OFF_ANIMATION, SCREEN_OFF_CRT);
                 mScreenOnAnimation = Settings.Global.getInt(cr,
                         Settings.Global.SCREEN_ON_ANIMATION, 0) != 0;
                 if (mPowerState != null) {
@@ -658,7 +658,7 @@ final class DisplayPowerController implements AutomaticBrightnessController.Call
                 Settings.Global.SCREEN_ON_ANIMATION),
                 false, observer, UserHandle.USER_ALL);
         mScreenOffAnimation = Settings.Global.getInt(cr,
-                Settings.Global.SCREEN_OFF_ANIMATION, 1);
+                Settings.Global.SCREEN_OFF_ANIMATION, SCREEN_OFF_CRT);
         mScreenOnAnimation = Settings.Global.getInt(cr,
                 Settings.Global.SCREEN_ON_ANIMATION, 0) != 0;
 