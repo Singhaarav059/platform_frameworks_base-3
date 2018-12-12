efaf66e271fbf037c7caa0ac627a0cb6f8fcdf63
SpiritCroc <dev@spiritcroc.de>
2018-12-12 17:46:50 +0100

    Network traffic: fix static color usage
    After setting a static color, we don't want to react to onDarkChanged
    anymore.
    This fixes wrong color in quick settings when qs have different color
    scheme than status bar.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -74,6 +74,7 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
     private int mVisibleState = -1;
     private boolean mTrafficVisible = false;
     private boolean mSystemIconVisible = true;
+    private boolean mColorIsStatic = false;
 
     private boolean mScreenOn = true;
 
@@ -355,6 +356,9 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
 
     @Override
     public void onDarkChanged(Rect area, float darkIntensity, int tint) {
+        if (mColorIsStatic) {
+            return;
+        }
         mTintColor = DarkIconDispatcher.getTint(area, this, tint);
         setTextColor(mTintColor);
         updateTrafficDrawable();
@@ -405,6 +409,7 @@ public class NetworkTraffic extends TextView implements StatusIconDisplayable {
 
     @Override
     public void setStaticDrawableColor(int color) {
+        mColorIsStatic = true;
         mTintColor = color;
         setTextColor(mTintColor);
         updateTrafficDrawable();
