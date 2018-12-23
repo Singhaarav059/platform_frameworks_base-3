nitrogen-project/android_frameworks_base

QS: add right/left padding even if it's 0 for collapsed  statusbar
For devices without a rounded display we are setting rounded_corner_content_padding
to 0 to avoid useless left/right padding in the statusbar.
But this looks odd in the expanded statusbar / qs panel because other elements
like the brightness slider buttons or the date have the padding anyway.

We can handle those cases separately.

Thanks @anirudhgupta109 for the suggestion.

packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
@@ -108,10 +108,12 @@ public class ScreenDecorations extends SystemUI implements Tunable {
                 R.dimen.rounded_corner_content_padding);
         int padding_alt = mContext.getResources().getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding_alt);
+        int qsPadding = mContext.getResources().getDimensionPixelSize(
+                R.dimen.qs_corner_content_padding);
         if (padding != 0 && Build.PRODUCT.equals("taimen")) {
-            setupPadding(padding);
+            setupPadding(padding, qsPadding);
         } else {
-            setupPadding(padding_alt);
+            setupPadding(padding_alt, qsPadding);
         }
 
         mDisplayListener = new DisplayManager.DisplayListener() {
@@ -284,7 +286,7 @@ public class ScreenDecorations extends SystemUI implements Tunable {
                 com.android.internal.R.bool.config_fillMainBuiltInDisplayCutout);
     }
 
-    private void setupPadding(int padding) {
+    private void setupPadding(int padding, int qsPadding) {
         // Add some padding to all the content near the edge of the screen.
         StatusBar sb = getComponent(StatusBar.class);
         View statusBar = (sb != null ? sb.getStatusBarWindow() : null);
@@ -295,8 +297,10 @@ public class ScreenDecorations extends SystemUI implements Tunable {
             FragmentHostManager fragmentHostManager = FragmentHostManager.get(statusBar);
             fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
                     new TunablePaddingTagListener(padding, R.id.status_bar));
-            fragmentHostManager.addTagListener(QS.TAG,
-                    new TunablePaddingTagListener(padding, R.id.header));
+            if (qsPadding != 0) {
+                fragmentHostManager.addTagListener(QS.TAG,
+                        new TunablePaddingTagListener(qsPadding, R.id.header));
+            }
         }
     }
 

Singhaarav059/platform_frameworks_base-3

packages/SystemUI/src/com/android/systemui/ScreenDecorations.java
@@ -371,15 +371,17 @@ public class ScreenDecorations extends SystemUI implements Tunable {
                 R.dimen.rounded_corner_content_padding);
         int padding_alt = mContext.getResources().getDimensionPixelSize(
                 R.dimen.rounded_corner_content_padding_alt);
-        if (padding != 0 && Build.PRODUCT.equals("taimen")) {
-            setupStatusBarPadding(padding);
+        int qsPadding = mContext.getResources().getDimensionPixelSize(
+                R.dimen.qs_corner_content_padding);
+	if (padding != 0 && Build.PRODUCT.equals("taimen")) {
+            setupStatusBarPadding(padding, qsPadding);
         } else {
-            setupStatusBarPadding(padding_alt);
+            setupStatusBarPadding(padding_alt, qsPadding);
         }
 
     }
 
-    private void setupStatusBarPadding(int padding) {
+    private void setupStatusBarPadding(int padding, int qsPadding) {
         // Add some padding to all the content near the edge of the screen.
         StatusBar sb = getComponent(StatusBar.class);
         View statusBar = (sb != null ? sb.getStatusBarWindow() : null);
@@ -390,8 +392,10 @@ public class ScreenDecorations extends SystemUI implements Tunable {
             FragmentHostManager fragmentHostManager = FragmentHostManager.get(statusBar);
             fragmentHostManager.addTagListener(CollapsedStatusBarFragment.TAG,
                     new TunablePaddingTagListener(padding, R.id.status_bar));
-            fragmentHostManager.addTagListener(QS.TAG,
-                    new TunablePaddingTagListener(padding, R.id.header));
+            if (qsPadding != 0) {
+                fragmentHostManager.addTagListener(QS.TAG,
+                        new TunablePaddingTagListener(qsPadding, R.id.header));
+            }
         }
     }
 