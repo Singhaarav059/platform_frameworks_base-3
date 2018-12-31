3a48c0150118c58fec4fd0894aa9ea43902cd469
Alex Cruz <du.alexcruz@gmail.com>
2018-12-31 05:05:49 -0600

    Ensure the clockview is aligned correctly each time
    Previously if you switched from Analog Clock to any of the Sammy style
    clock views, you would notice sometimes that the view wasn't aligned
    correctly. Shutting the device off and turning it back on would fix this
    issue but that's just not the right fix for this.
    
    - Add back letterSpacing in clock view. It was removed by mistake
    
    Change-Id: Iae3d8fd875bef11f485c8ec8167861bb63fdef55

--- packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
+++ packages/SystemUI/src/com/android/keyguard/KeyguardStatusView.java
@@ -38,6 +38,7 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.util.Slog;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.CustomAnalogClock;
 import android.widget.GridLayout;
@@ -1212,10 +1213,12 @@ public class KeyguardStatusView extends GridLayout implements
             default:
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(true);
+                mClockView.setGravity(Gravity.CENTER);
                 break;
             case 1: // digital (bold)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(true);
+                mClockView.setGravity(Gravity.CENTER);
                 break;
             case 2: // analog
                 mClockView.setVisibility(View.GONE);
@@ -1224,10 +1227,12 @@ public class KeyguardStatusView extends GridLayout implements
             case 3: // sammy
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(false);
+                mClockView.setGravity(Gravity.CENTER);
                 break;
             case 4: // sammy (bold)
                 params.addRule(RelativeLayout.BELOW, R.id.clock_view);
                 mClockView.setSingleLine(false);
+                mClockView.setGravity(Gravity.CENTER);
                 break;
         }
 
