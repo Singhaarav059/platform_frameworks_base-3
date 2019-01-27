c290e3e8e4abe372e9cf9985070459dd8dcc6732
Ezio Lacandia Bijelkic <brabus84@gmail.com>
2019-01-27 16:06:14 +0000

    Ambient brightness values: use a custom system setting [1/2]
    part 2 is in CustomDoze package
    
    Change-Id: I621125a2669d9c2180341704b046d2089a996b86

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4353,6 +4353,13 @@ public final class Settings {
          */
         public static final String NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD = "network_traffic_autohide_threshold";
 
+        /**
+         * Ambient screen settings
+         * "screen_brightness_array=0:1:2:3:4"
+         * @hide
+         */
+        public static final String ALWAYS_ON_DISPLAY_CONSTANTS_CUST = "always_on_display_constants_cust";
+
 	/**
          * Whether to hide the lockscreen clock
          * @hide
