768c7d9d4b5702cd6510ea310d32b0e065bba474
Alex <du.alexcruz@gmail.com>
2019-02-02 12:57:40 -0600

    Network traffic: Retain our notch check
    Even with adding the network traffic indicator to the statusbar icon
    list and holder, the indicators sometime glitch out with notch devices.
    With this check we ensure that nothing gets out of place while still
    playing nice with everything else.
    
    Change-Id: If3817d7a95a46c1a9d0555b606dcd1ce19014eff
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>
    
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTrafficSB.java
@@ -29,6 +29,7 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.TextView;
 
+import com.android.internal.util.colt.ColtUtils;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
 import com.android.systemui.plugins.DarkIconDispatcher;
@@ -388,7 +389,8 @@ public class NetworkTrafficSB extends TextView implements StatusIconDisplayable 
     }
 
     private void updateVisibility() {
-        if (mIsEnabled && mTrafficVisible && mSystemIconVisible) {
+        if (!ColtUtils.hasNotch(mContext) && mIsEnabled &&
+                mTrafficVisible && mSystemIconVisible) {
             setVisibility(View.VISIBLE);
         } else {
             setVisibility(View.GONE);
