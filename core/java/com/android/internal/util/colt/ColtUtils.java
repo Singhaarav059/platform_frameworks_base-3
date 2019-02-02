85fd1f2ba56763ce1fd661af52a883b07ca356df
Alex Cruz <du.alexcruz@gmail.com>
2019-02-02 13:21:07 -0200

    Utils: Add method to determine battery temp
    Change-Id: Id0b8addc460afae030eac0a2bf064fb21aa2c4dc

--- core/java/com/android/internal/util/colt/ColtUtils.java
+++ core/java/com/android/internal/util/colt/ColtUtils.java
@@ -20,6 +20,8 @@ import android.app.ActivityManager;
 import android.app.AlertDialog; 
 import android.app.IActivityManager;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.content.Intent;
@@ -27,6 +29,8 @@ import android.content.pm.PackageManager;
 import android.hardware.input.InputManager;
 import android.os.Handler;
 import android.os.Looper;
+import android.hardware.fingerprint.FingerprintManager;
+import android.os.BatteryManager;
 import android.os.PowerManager;
 import android.os.RemoteException;
 import android.os.ServiceManager;
@@ -240,4 +244,18 @@ public class ColtUtils {
             }
         }
     }
+
+    public static String batteryTemperature(Context context, Boolean ForC) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(
+                Intent.ACTION_BATTERY_CHANGED));
+        float  temp = ((float) (intent != null ? intent.getIntExtra(
+                BatteryManager.EXTRA_TEMPERATURE, 0) : 0)) / 10;
+        // Round up to nearest number
+        int c = (int) ((temp) + 0.5f);
+        float n = temp + 0.5f;
+        // Use boolean to determine celsius or fahrenheit
+        return String.valueOf((n - c) % 2 == 0 ? (int) temp :
+                ForC ? c * 9/5 + 32 + "°F" :c + "°C");
+    }
+
 }
