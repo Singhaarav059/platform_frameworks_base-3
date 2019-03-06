f047e510f4519ed040b1f81c8e2f9c1b0122113c
Davor Bertovic <davor@losinj.com>
2019-03-06 23:09:18 +0000

    Sensor block per-package switch [1/2]
    Change-Id: I3a5cb8820ca6815c7140c25368e525faa2ecc5e6
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6193,6 +6193,12 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Sensor block per-package
+         * @hide
+         */
+        public static final String SENSOR_BLOCK = "sensor_block";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
