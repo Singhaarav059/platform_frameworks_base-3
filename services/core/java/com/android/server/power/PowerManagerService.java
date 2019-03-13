76e8cff430894cc34d095afd9b1db2b83d7c157d
Shubham Singh <coolsks94@gmail.com>
2019-03-13 21:25:10 +0100

    PowerManager: Don't bother devices not using wait for MPCTL
    Change-Id: I8951adae0da29a2ec7d512ab601d919d90455a07
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- services/core/java/com/android/server/power/PowerManagerService.java
+++ services/core/java/com/android/server/power/PowerManagerService.java
@@ -715,7 +715,7 @@ public final class PowerManagerService extends SystemService
                 Process.THREAD_PRIORITY_DISPLAY, false /*allowIo*/);
         mHandlerThread.start();
         mHandler = new PowerManagerHandler(mHandlerThread.getLooper());
-        if (mContext.getResources().getBoolean(com.android.internal.R.bool.config_waitForMpctlOnBoot)) {
+        if (isWaitForMpctlOnBootEnabled()) {
             mMpctlReady = false;
             mWaitMpctlThread = new Thread(() -> {
                 int retries = 20;
@@ -794,7 +794,7 @@ public final class PowerManagerService extends SystemService
                 Process.THREAD_PRIORITY_DISPLAY, false /*allowIo*/);
         mHandlerThread.start();
         mHandler = new PowerManagerHandler(mHandlerThread.getLooper());
-        if (mContext.getResources().getBoolean(com.android.internal.R.bool.config_waitForMpctlOnBoot)) {
+        if (isWaitForMpctlOnBootEnabled()) {
             mMpctlReady = false;
             mWaitMpctlThread = new Thread(() -> {
                 int retries = 20;
@@ -856,14 +856,21 @@ public final class PowerManagerService extends SystemService
         Watchdog.getInstance().addThread(mHandler);
     }
 
+    public boolean isWaitForMpctlOnBootEnabled() {
+        return mContext.getResources().getBoolean(com.android.internal.R.bool.config_waitForMpctlOnBoot);
+    }
+
     @Override
     public void onBootPhase(int phase) {
         synchronized (mLock) {
             if (phase == PHASE_THIRD_PARTY_APPS_CAN_START) {
                 incrementBootCount();
+            } else if ((phase == PHASE_BOOT_COMPLETED && !isWaitForMpctlOnBootEnabled()) || (phase == PHASE_BOOT_COMPLETED && isWaitForMpctlOnBootEnabled() && !mMpctlReady)) {
 
-            } else if (phase == PHASE_BOOT_COMPLETED && !mMpctlReady) {
-                mWaitMpctlThread.start();
+                if (isWaitForMpctlOnBootEnabled() && !mMpctlReady) {
+                  mWaitMpctlThread.start();
+                }
+
                 final long now = SystemClock.uptimeMillis();
                 mBootCompleted = true;
                 mDirty |= DIRTY_BOOT_COMPLETED;
