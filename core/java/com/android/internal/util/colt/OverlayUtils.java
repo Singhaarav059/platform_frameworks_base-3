42f302f82415ff76602bc93d51e5ce75b6b8883e
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-06-02 11:22:30 -0300

    fwb: Add overlay for SetupWizard
    Enable if aod available
    
    Change-Id: I5ec307a313982412b1d21243c3428d0091821e8a

--- core/java/com/android/internal/util/colt/OverlayUtils.java
+++ core/java/com/android/internal/util/colt/OverlayUtils.java
@@ -16,5 +16,5 @@
 package com.android.internal.util.colt;
 
 public class OverlayUtils {
-    public static String[] AllPackages = {};
+    public static String[] AllPackages = {"com.google.android.pixel.setupwizard.overlay"};
 }
