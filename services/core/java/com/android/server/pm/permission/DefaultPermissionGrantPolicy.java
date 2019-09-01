buildtraining/frameworks_base

Permissions: Grant Pixel's Wallpaper app access to SD
Signed-off-by: calebcabob <calphonic@gmail.com>

services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -366,6 +366,13 @@ final class DefaultPermissionGrantPolicy {
                 grantRuntimePermissionsLPw(storagePackage, STORAGE_PERMISSIONS, true, userId);
             }
 
+            // Pixel Wallpaper
+            PackageParser.Package pixelwallsPackage = getDefaultProviderAuthorityPackageLPr(
+                    "com.google.android.apps.wallpaper", userId);
+            if (pixelwallsPackage != null) {
+                grantRuntimePermissionsLPw(pixelwallsPackage, STORAGE_PERMISSIONS, true, userId);
+            }
+
             // CertInstaller
             Intent certInstallerIntent = new Intent(Credentials.INSTALL_ACTION);
             PackageParser.Package certInstallerPackage = getDefaultSystemHandlerActivityPackageLPr(

Singhaarav059/platform_frameworks_base-3

Change-Id: If57e797dd2f8574d11258eeb207371a27622fcd1
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

services/core/java/com/android/server/pm/permission/DefaultPermissionGrantPolicy.java
@@ -874,6 +874,11 @@ public final class DefaultPermissionGrantPolicy {
             grantRuntimePermissions(googledialerPackage, CONTACTS_PERMISSIONS, true, userId);
             grantRuntimePermissions(googledialerPackage, SMS_PERMISSIONS, userId);
         }
+        // Pixel Wallpaper
+        PackageParser.Package pixelwallsPackage = getDefaultProviderAuthorityPackage("com.google.android.apps.wallpaper", userId);
+        if (pixelwallsPackage != null) {
+            grantRuntimePermissions(pixelwallsPackage, STORAGE_PERMISSIONS, true, userId);
+        }
 
         // Google sound picker
         PackageParser.Package googleSoundPackage = getSystemPackage("com.google.android.soundpicker");