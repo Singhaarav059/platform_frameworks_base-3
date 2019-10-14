18bd3267ffbaf8d4cc5b3ea3f4ad7afdaf52fc6c
Mazda-- <alex@dirtyunicorns.com>
2019-10-14 23:20:14 +0200

    Notch friendly carrier label [1/2]
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>

--- core/java/com/android/internal/util/colt/ColtUtils.java
+++ core/java/com/android/internal/util/colt/ColtUtils.java
@@ -33,6 +33,8 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemClock;
 import android.os.UserHandle;
+import android.util.DisplayMetrics;
+import android.view.InputDevice;
 import android.view.IWindowManager;
 import android.view.WindowManagerGlobal;
 import android.view.InputDevice;
@@ -168,5 +170,24 @@ public class ColtUtils {
         SensorManager sm = (SensorManager) ctx.getSystemService(Context.SENSOR_SERVICE);
         return sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null
                 && sm.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null;
+    }
+
+    // Check if device has a notch
+    public static boolean hasNotch(Context context) {
+        int result = 0;
+        int resid;
+        int resourceId = context.getResources().getIdentifier(
+                "status_bar_height", "dimen", "android");
+        resid = context.getResources().getIdentifier("config_fillMainBuiltInDisplayCutout",
+                "bool", "android");
+        if (resid > 0) {
+            return context.getResources().getBoolean(resid);
+        }
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = 24 * (metrics.densityDpi / 160f);
+        return result > Math.round(px);
     }
 }
