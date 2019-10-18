eec98661c00a58b14e3d96ba0b4e302823d58439
SuperDroidBond <superdroidbond@yahoo.com>
2019-10-18 21:15:51 +0530

    Notification LED: Add support for showing in Zen Mode

--- services/core/java/com/android/server/notification/NotificationManagerService.java
+++ services/core/java/com/android/server/notification/NotificationManagerService.java
@@ -6059,7 +6059,10 @@ public class NotificationManagerService extends SystemService {
         if (isInCall() || mScreenOn) {
             return false;
         }
-
+        // Omni Lights
+        if (!record.isIntercepted() || record.isIntercepted() && record.shouldLightOnZen()) {
+            return false;
+        }
         return true;
     }
 
