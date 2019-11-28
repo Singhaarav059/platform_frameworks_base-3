e2a3bafa1dcbb88dff201ae5c42eca6bd52ee4ac
ezio84 <brabus84@gmail.com>
2019-11-28 20:03:27 +0530

    base: Notch friendly network traffic [1/2]
    Based on https://github.com/ezio84/abc_frameworks_base/commit/4ee2f30df98de77c432a70809aad4c40db845db5
    
    @SKULSHADY:
    - Use a single setting to switch between statusbar and qs header
    - Hide the statusbar option for devices with notch
    - Cleanup and adapt to our OOS style traffic indicator
    - Fixup the padding and layout in the qs header
    
    @SpiritCroc:
    - Update to work with our normal status bar network traffic and allow user selection
    
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4972,10 +4972,10 @@ public final class Settings {
 	        new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);;
 
         /**
-         * Wheter to show network traffic indicator in statusbar
+         * Wheter to show network traffic indicator
          * @hide
          */
-        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+        public static final String NETWORK_TRAFFIC_LOCATION = "network_traffic_location";
 
         /**
          * Network traffic inactivity threshold (default is 1 kBs)
