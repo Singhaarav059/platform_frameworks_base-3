cb80e052b16eac49faeab93f4d696ac43d61106b
00day0 <therandomuser11@gmail.com>
2019-12-17 16:52:31 +0100

    Accents: add MIUI accent color
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -20,6 +20,7 @@ LOCAL_REQUIRED_MODULES := \
 	AccentColorSluttyPinkOverlay \
 	AccentColorBlueMondayOverlay \
 	AccentColorGoldenShowerOverlay \
+	AccentColorMIUIOverlay \
 	AccentColorTealOverlay \
 	AccentColorRedOverlay \
 	AccentColorQGreenOverlay \
