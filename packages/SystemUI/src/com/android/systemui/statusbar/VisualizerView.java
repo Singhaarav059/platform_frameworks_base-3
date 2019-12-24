a9d72d7abb6b16751492ad017c6112b69f5d44d7
Harsh Shandilya <msfjarvis@gmail.com>
2019-12-24 23:35:07 +0530

    VisualizerView: Don't add null end actions to animators
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/VisualizerView.java
@@ -475,13 +475,11 @@ public class VisualizerView extends View
                 AsyncTask.execute(mLinkVisualizer);
                 animate()
                         .alpha(0.40f)
-                        .withEndAction(null)
                         .setDuration(800);
             } else {
                 mPaint.setColor(mColor);
                 animate()
                         .alpha(0.40f)
-                        .withEndAction(null)
                         .setDuration(800);
             }
         } else if (getVisibility() == View.VISIBLE && mVisible && mPlaying
@@ -491,14 +489,12 @@ public class VisualizerView extends View
                 AsyncTask.execute(mLinkVisualizer);
                 animate()
                         .alpha(1f)
-                        .withEndAction(null)
                         .setDuration(800);
                 if (mLavaLampEnabled) mLavaLamp.start();
             } else {
                 mPaint.setColor(mColor);
                 animate()
                         .alpha(1f)
-                        .withEndAction(null)
                         .setDuration(800);
             }
         } else {
