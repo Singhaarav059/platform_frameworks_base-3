1512ff1bada582775e9139b17a956dbb1909e4ff
NurKeinNeid <mralexman3000@gmail.com>
2020-01-01 01:02:21 +0100

    Partially revert "overlay: Rename the icon pack overlays to pixel names"
     * this partially reverts ed538cfa5b3aac9d0eb78c57a8e7b3fcdcad76b4
    
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -51,17 +51,17 @@ LOCAL_REQUIRED_MODULES := \
 	FontOnePlusSlateOverlay \
 	IconPackCircularAndroidOverlay \
 	IconPackCircularPixelLauncherOverlay \
-	IconPackCircularPixelThemePickerOverlay \
+	IconPackCircularThemePickerOverlay \
 	IconPackCircularSettingsOverlay \
 	IconPackCircularSystemUIOverlay \
 	IconPackFilledAndroidOverlay \
 	IconPackFilledPixelLauncherOverlay \
-	IconPackFilledPixelThemePickerOverlay \
+	IconPackFilledThemePickerOverlay \
 	IconPackFilledSettingsOverlay \
 	IconPackFilledSystemUIOverlay \
 	IconPackRoundedAndroidOverlay \
 	IconPackRoundedPixelLauncherOverlay \
-	IconPackRoundedPixelThemePickerOverlay \
+	IconPackRoundedThemePickerOverlay \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerUIOverlay \
