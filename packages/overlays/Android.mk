f32c2086c28e2cdb17c48705c6145f3878957118
Pranav Vashi <neobuddy89@gmail.com>
2020-01-12 10:09:14 +0530

    Add Comfortaa font [1/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -32,6 +32,7 @@ LOCAL_REQUIRED_MODULES := \
         FontSurferSourceOverlay \
 	FontArbutusSourceOverlay \
 	FontArvoLatoOverlay \
+	FontComfortaaSourceOverlay \
 	FontComicSansSourceOverlay \
 	FontGoogleSansSourceOverlay \
 	FontNotoSerifSourceOverlay \
