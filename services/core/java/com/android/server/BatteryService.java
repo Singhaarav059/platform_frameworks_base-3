332ab2378349cc58085cc5ae99b1074e2cb12c8f
Alex <du.alexcruz@gmail.com>
2020-01-21 02:54:50 -0500

    Dash & Warp Charging: Remove bool dependency
    We're already checking for the file path to determine if dash charging
    is available. There's no reason for added check. Especially a check that
    really didn't make sense in the first place. At no point are we going to
    turn this off on a device that has support for it.
    
    Change-Id: I881ff0ba3f88da4c85858e6667713e701637348b
    Signed-off-by: Joey Huab <joey@evolution-x.org>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- services/core/java/com/android/server/BatteryService.java
+++ services/core/java/com/android/server/BatteryService.java
@@ -176,11 +176,9 @@ public final class BatteryService extends SystemService {
     private boolean mBatteryLevelLow;
 
     private boolean mDashCharger;
-    private boolean mHasDashCharger;
     private boolean mLastDashCharger;
 
     private boolean mWarpCharger;
-    private boolean mHasWarpCharger;
     private boolean mLastWarpCharger;
 
     private long mDischargeStartTime;
@@ -224,11 +222,6 @@ public final class BatteryService extends SystemService {
         mLed = new Led(context, getLocalService(LightsManager.class));
         mBatteryStats = BatteryStatsService.getService();
         mActivityManagerInternal = LocalServices.getService(ActivityManagerInternal.class);
-
-        mHasDashCharger = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_hasDashCharger);
-        mHasWarpCharger = mContext.getResources().getBoolean(
-                com.android.internal.R.bool.config_hasWarpCharger);
 
         mCriticalBatteryLevel = mContext.getResources().getInteger(
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel);
@@ -600,8 +593,8 @@ public final class BatteryService extends SystemService {
         shutdownIfNoPowerLocked();
         shutdownIfOverTempLocked();
 
-        mDashCharger = mHasDashCharger && isDashCharger();
-        mWarpCharger = mHasWarpCharger && isWarpCharger();
+        mDashCharger = isDashCharger();
+        mWarpCharger = isWarpCharger();
 
         if (force || (mHealthInfo.batteryStatus != mLastBatteryStatus ||
                 mHealthInfo.batteryHealth != mLastBatteryHealth ||
