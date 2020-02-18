Havoc-OS/android_frameworks_base

base: SystemUI: add QS rows and landscape rows config
with inspiration from ezio84

idoybh edit: adapted to android10

Change-Id: I980a1ab2ed024f467367b222ab408ccfe1e1882f

packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
@@ -6,10 +6,13 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -432,9 +435,10 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
             };
 
     public static class TilePage extends TileLayout {
-
+        private int mRows;
         public TilePage(Context context, AttributeSet attrs) {
             super(context, attrs);
+            mRows = getRows();
         }
 
         public boolean isFull() {
@@ -453,7 +457,28 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
             final int sidePadding = getContext().getResources().getDimensionPixelSize(
                     R.dimen.notification_side_paddings);
             setPadding(sidePadding, 0, sidePadding, 0);
-            return super.updateResources();
+            final int rows = getRows();
+            boolean changed = rows != mRows;
+            if (changed) {
+                mRows = rows;
+                requestLayout();
+            }
+            return super.updateResources() || changed;
+        }
+
+        private int getRows() {
+            final Resources res = getContext().getResources();
+            final ContentResolver resolver = mContext.getContentResolver();
+
+            if (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                return Settings.System.getIntForUser(resolver,
+                        Settings.System.QS_LAYOUT_ROWS, 3,
+                        UserHandle.USER_CURRENT);
+            }
+
+            return Settings.System.getIntForUser(resolver,
+                        Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                        UserHandle.USER_CURRENT);
         }
     }
 
@@ -499,6 +524,7 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
     public void updateSettings() {
         for (int i = 0; i < mPages.size(); i++) {
             mPages.get(i).updateSettings();
+            mPages.get(i).updateResources();
         }
         distributeTiles();
     }
@@ -506,6 +532,10 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
     @Override
     public int getNumColumns() {
         return mPages.get(0).getNumColumns();
+    }
+
+    public int getNumRows() {
+        return mPages.get(0).getNumRows();
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -1,6 +1,7 @@
 package com.android.systemui.qs;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.UserHandle;
@@ -34,7 +35,7 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
     protected final ArrayList<TileRecord> mRecords = new ArrayList<>();
     private int mCellMarginTop;
     private boolean mListening;
-    protected int mMaxAllowedRows = 3;
+    protected int mMaxAllowedRows = 4;
 
     public TileLayout(Context context) {
         this(context, null);
@@ -137,6 +138,8 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
      * @param tilesCount Upper limit on the number of tiles to show. to prevent empty rows.
      */
     public boolean updateMaxRows(int heightMeasureSpec, int tilesCount) {
+        final Resources res = getContext().getResources();
+        final ContentResolver resolver = mContext.getContentResolver();
         final int availableHeight = MeasureSpec.getSize(heightMeasureSpec) - mCellMarginTop
                 + mCellMarginVertical;
         final int previousRows = mRows;
@@ -149,6 +152,19 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         if (mRows > (tilesCount + mColumns - 1) / mColumns) {
             mRows = (tilesCount + mColumns - 1) / mColumns;
         }
+        int rowsSet;
+        if (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            rowsSet = Settings.System.getIntForUser(resolver,
+                    Settings.System.QS_LAYOUT_ROWS, 3,
+                    UserHandle.USER_CURRENT);
+        }
+        else {
+            rowsSet = Settings.System.getIntForUser(resolver,
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 3,
+                    UserHandle.USER_CURRENT);
+        }
+        if (mRows > rowsSet)
+            mRows = rowsSet;
         return previousRows != mRows;
     }
 
@@ -214,6 +230,12 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         int columnsLandscape = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE, defaultColumns,
                 UserHandle.USER_CURRENT);
+        int rows = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS, 3,
+                UserHandle.USER_CURRENT);
+        int rowsLandscape = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                UserHandle.USER_CURRENT);
         boolean showTitles = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT) == 1;
@@ -227,11 +249,20 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
             mShowTitles = showTitles;
             requestLayout();
         }
+        if (mRows != (isPortrait ? rows : rowsLandscape) || mShowTitles != showTitles) {
+            mRows = isPortrait ? rows : rowsLandscape;
+            mShowTitles = showTitles;
+            requestLayout();
+        }
     }
 
     @Override
     public int getNumColumns() {
         return mColumns;
+    }
+
+    public int getNumRows() {
+        return mRows;
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -99,6 +99,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private Menu mColumnsSubMenu;
     private Menu mColumnsLandscapeSubMenu;
     private Menu mQsColumnsSubMenu;
+    private Menu mRowsSubMenu;
+    private Menu mRowsLandscapeSubMenu;
     private boolean mHeaderImageEnabled;
 
     @Inject
@@ -135,6 +137,14 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         MenuItem menuItemQs = mToolbar.getMenu().findItem(R.id.menu_item_qs_columns);
         if (menuItemQs != null) {
             mQsColumnsSubMenu = menuItemQs.getSubMenu();
+        }
+        MenuItem menuItemRows = mToolbar.getMenu().findItem(R.id.menu_item_rows);
+        if (menuItem != null) {
+            mRowsSubMenu = menuItem.getSubMenu();
+        }
+        MenuItem menuItemRowsLand = mToolbar.getMenu().findItem(R.id.menu_item_rows_landscape);
+        if (menuItemLand != null) {
+            mRowsLandscapeSubMenu = menuItemLand.getSubMenu();
         }
         int qsTitlesValue = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.QS_TILE_TITLE_VISIBILITY, 1,
@@ -271,6 +281,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             }
             if (mQsColumnsSubMenu != null) {
                 mQsColumnsSubMenu.close();
+            if (mRowsSubMenu != null) {
+                mRowsSubMenu.close();
+            }
+            if (mRowsLandscapeSubMenu != null) {
+                mRowsLandscapeSubMenu.close();
             }
             mToolbar.dismissPopupMenus();
             setCustomizing(false);
@@ -284,6 +299,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             mNotifQsContainer.setCustomizerShowing(false);
             mKeyguardMonitor.removeCallback(mKeyguardCallback);
             updateNavColors();
+            }
         }
     }
 
@@ -356,6 +372,24 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         } else if (id ==  R.id.menu_item_qs_columns_auto) {
             Settings.System.putIntForUser(mContext.getContentResolver(),
                     Settings.System.QS_QUICKBAR_COLUMNS, -1, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_one) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS, 1, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_two) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS, 2, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_three) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS, 3, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_four) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS, 4, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_landscape_one) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 1, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_landscape_two) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2, UserHandle.USER_CURRENT);
         }
 
         updateSettings();
@@ -481,13 +515,21 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         int columnsLandscape = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.QS_LAYOUT_COLUMNS_LANDSCAPE, defaultColumns,
                 UserHandle.USER_CURRENT);
+        int rows = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS, 3,
+                UserHandle.USER_CURRENT);
+        int rowsLandscape = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                UserHandle.USER_CURRENT);
         boolean showTitles = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT) == 1;
         mTileAdapter.setColumnCount(isPortrait ? columns : columnsLandscape);
+        mTileAdapter.setRowsCount(isPortrait ? rows : rowsLandscape);
         mTileAdapter.setHideLabel(!showTitles);
         mLayout.setSpanCount(isPortrait ? columns : columnsLandscape);
         updateColumnsMenu(defaultColumns);
+        updateRowsMenu();
         mHeaderImageEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, 0,
                 UserHandle.USER_CURRENT) == 1;
@@ -537,5 +579,29 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         menuItemEight.setChecked(qsColumns == 8);
         MenuItem menuItemAuto = mToolbar.getMenu().findItem(R.id.menu_item_qs_columns_auto);
         menuItemAuto.setChecked(qsColumns == -1);
+    }
+
+    private void updateRowsMenu() {
+        /* TODO add a value a vendor can change to increase max rows on big ass
+              screens  */
+        int rows = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS, 3,
+                UserHandle.USER_CURRENT);
+        MenuItem menuItemOne = mToolbar.getMenu().findItem(R.id.menu_item_rows_one);
+        menuItemOne.setChecked(rows == 1);
+        MenuItem menuItemTwo = mToolbar.getMenu().findItem(R.id.menu_item_rows_two);
+        menuItemTwo.setChecked(rows == 2);
+        MenuItem menuItemThree = mToolbar.getMenu().findItem(R.id.menu_item_rows_three);
+        menuItemThree.setChecked(rows == 3);
+        MenuItem menuItemFour = mToolbar.getMenu().findItem(R.id.menu_item_rows_four);
+        menuItemFour.setChecked(rows == 4);
+
+        int rowsLandscape = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                UserHandle.USER_CURRENT);
+        menuItemOne = mToolbar.getMenu().findItem(R.id.menu_item_rows_landscape_one);
+        menuItemOne.setChecked(rowsLandscape == 1);
+        menuItemTwo = mToolbar.getMenu().findItem(R.id.menu_item_rows_landscape_two);
+        menuItemTwo.setChecked(rowsLandscape == 2);
     }
 }

Singhaarav059/platform_frameworks_base-3

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

packages/SystemUI/src/com/android/systemui/qs/PagedTileLayout.java
@@ -6,10 +6,13 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -427,9 +430,10 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
             };
 
     public static class TilePage extends TileLayout {
-
+        private int mRows;
         public TilePage(Context context, AttributeSet attrs) {
             super(context, attrs);
+            mRows = getRows();
         }
 
         public boolean isFull() {
@@ -448,7 +452,28 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
             final int sidePadding = getContext().getResources().getDimensionPixelSize(
                     R.dimen.notification_side_paddings);
             setPadding(sidePadding, 0, sidePadding, 0);
-            return super.updateResources();
+            final int rows = getRows();
+            boolean changed = rows != mRows;
+            if (changed) {
+                mRows = rows;
+                requestLayout();
+            }
+            return super.updateResources() || changed;
+        }
+
+        private int getRows() {
+            final Resources res = getContext().getResources();
+            final ContentResolver resolver = mContext.getContentResolver();
+
+            if (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+                return Settings.System.getIntForUser(resolver,
+                        Settings.System.OMNI_QS_LAYOUT_ROWS, 3,
+                        UserHandle.USER_CURRENT);
+            }
+
+            return Settings.System.getIntForUser(resolver,
+                        Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                        UserHandle.USER_CURRENT);
         }
     }
 
@@ -494,6 +519,7 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
     public void updateSettings() {
         for (int i = 0; i < mPages.size(); i++) {
             mPages.get(i).updateSettings();
+            mPages.get(i).updateResources();
         }
         distributeTiles();
     }
@@ -501,6 +527,10 @@ public class PagedTileLayout extends ViewPager implements QSTileLayout {
     @Override
     public int getNumColumns() {
         return mPages.get(0).getNumColumns();
+    }
+
+    public int getNumRows() {
+        return mPages.get(0).getNumRows();
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/qs/TileLayout.java
@@ -1,6 +1,7 @@
 package com.android.systemui.qs;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.UserHandle;
@@ -34,7 +35,7 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
     protected final ArrayList<TileRecord> mRecords = new ArrayList<>();
     private int mCellMarginTop;
     private boolean mListening;
-    protected int mMaxAllowedRows = 3;
+    protected int mMaxAllowedRows = 4;
 
     public TileLayout(Context context) {
         this(context, null);
@@ -138,6 +139,8 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
      * @param tilesCount Upper limit on the number of tiles to show. to prevent empty rows.
      */
     public boolean updateMaxRows(int heightMeasureSpec, int tilesCount) {
+        final Resources res = getContext().getResources();
+        final ContentResolver resolver = mContext.getContentResolver();
         final int availableHeight = MeasureSpec.getSize(heightMeasureSpec) - mCellMarginTop
                 + mCellMarginVertical;
         final int previousRows = mRows;
@@ -150,6 +153,19 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         if (mRows > (tilesCount + mColumns - 1) / mColumns) {
             mRows = (tilesCount + mColumns - 1) / mColumns;
         }
+        int rowsSet;
+        if (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            rowsSet = Settings.System.getIntForUser(resolver,
+                    Settings.System.OMNI_QS_LAYOUT_ROWS, 3,
+                    UserHandle.USER_CURRENT);
+        }
+        else {
+            rowsSet = Settings.System.getIntForUser(resolver,
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 3,
+                    UserHandle.USER_CURRENT);
+        }
+        if (mRows > rowsSet)
+            mRows = rowsSet;
         return previousRows != mRows;
     }
 
@@ -215,6 +231,12 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
         int columnsLandscape = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, defaultColumns,
                 UserHandle.USER_CURRENT);
+        int rows = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_ROWS, 3,
+                UserHandle.USER_CURRENT);
+        int rowsLandscape = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                UserHandle.USER_CURRENT);
         boolean showTitles = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT) == 1;
@@ -228,11 +250,20 @@ public class TileLayout extends ViewGroup implements QSTileLayout {
             mShowTitles = showTitles;
             requestLayout();
         }
+        if (mRows != (isPortrait ? rows : rowsLandscape) || mShowTitles != showTitles) {
+            mRows = isPortrait ? rows : rowsLandscape;
+            mShowTitles = showTitles;
+            requestLayout();
+        }
     }
 
     @Override
     public int getNumColumns() {
         return mColumns;
+    }
+
+    public int getNumRows() {
+        return mRows;
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/qs/customize/QSCustomizer.java
@@ -99,6 +99,8 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
     private Menu mColumnsLandscapeSubMenu;
     private Menu mQsColumnsSubMenu;
     private boolean mHeaderImageEnabled;
+    private Menu mRowsSubMenu;
+    private Menu mRowsLandscapeSubMenu;
 
     @Inject
     public QSCustomizer(Context context, AttributeSet attrs,
@@ -134,6 +136,14 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         MenuItem menuItemQs = mToolbar.getMenu().findItem(R.id.menu_item_qs_columns);
         if (menuItemQs != null) {
             mQsColumnsSubMenu = menuItemQs.getSubMenu();
+        }
+        MenuItem menuItemRows = mToolbar.getMenu().findItem(R.id.menu_item_rows);
+        if (menuItem != null) {
+            mRowsSubMenu = menuItem.getSubMenu();
+        }
+        MenuItem menuItemRowsLand = mToolbar.getMenu().findItem(R.id.menu_item_rows_landscape);
+        if (menuItemLand != null) {
+            mRowsLandscapeSubMenu = menuItemLand.getSubMenu();
         }
         int qsTitlesValue = Settings.System.getIntForUser(mContext.getContentResolver(),
                 Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
@@ -270,6 +280,11 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             }
             if (mQsColumnsSubMenu != null) {
                 mQsColumnsSubMenu.close();
+            if (mRowsSubMenu != null) {
+                mRowsSubMenu.close();
+            }
+            if (mRowsLandscapeSubMenu != null) {
+                mRowsLandscapeSubMenu.close();
             }
             mToolbar.dismissPopupMenus();
             setCustomizing(false);
@@ -283,6 +298,7 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
             mNotifQsContainer.setCustomizerShowing(false);
             mKeyguardMonitor.removeCallback(mKeyguardCallback);
             updateNavColors();
+            }
         }
     }
 
@@ -355,6 +371,24 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         } else if (id ==  R.id.menu_item_qs_columns_auto) {
             Settings.System.putIntForUser(mContext.getContentResolver(),
                     Settings.System.OMNI_QS_QUICKBAR_COLUMNS, -1, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_one) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_QS_LAYOUT_ROWS, 1, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_two) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_QS_LAYOUT_ROWS, 2, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_three) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_QS_LAYOUT_ROWS, 3, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_four) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_QS_LAYOUT_ROWS, 4, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_landscape_one) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 1, UserHandle.USER_CURRENT);
+        } else if (id == R.id.menu_item_rows_landscape_two) {
+            Settings.System.putIntForUser(mContext.getContentResolver(),
+                    Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2, UserHandle.USER_CURRENT);
         }
 
         updateSettings();
@@ -473,16 +507,24 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         int columnsLandscape = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_COLUMNS_LANDSCAPE, defaultColumns,
                 UserHandle.USER_CURRENT);
+        int rows = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_ROWS, 3,
+                UserHandle.USER_CURRENT);
+        int rowsLandscape = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                UserHandle.USER_CURRENT);
         boolean showTitles = Settings.System.getIntForUser(
                 mContext.getContentResolver(), Settings.System.OMNI_QS_TILE_TITLE_VISIBILITY, 1,
                 UserHandle.USER_CURRENT) == 1;
         mTileAdapter.setColumnCount(isPortrait ? columns : columnsLandscape);
+        mTileAdapter.setRowsCount(isPortrait ? rows : rowsLandscape);
         mTileAdapter.setHideLabel(!showTitles);
         mLayout.setSpanCount(isPortrait ? columns : columnsLandscape);
         updateColumnsMenu(defaultColumns);
         mHeaderImageEnabled = Settings.System.getIntForUser(mContext.getContentResolver(),
 		Settings.System.OMNI_STATUS_BAR_CUSTOM_HEADER, 0,
                 UserHandle.USER_CURRENT) == 1;
+        updateRowsMenu();
     }
 
     private void updateColumnsMenu(int defaultColumns) {
@@ -525,5 +567,29 @@ public class QSCustomizer extends LinearLayout implements OnMenuItemClickListene
         menuItemEight.setChecked(qsColumns == 8);
         MenuItem menuItemAuto = mToolbar.getMenu().findItem(R.id.menu_item_qs_columns_auto);
         menuItemAuto.setChecked(qsColumns == -1);
+    }
+
+    private void updateRowsMenu() {
+        /* TODO add a value a vendor can change to increase max rows on big ass
+              screens  */
+        int rows = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.OMNI_QS_LAYOUT_ROWS, 3,
+                UserHandle.USER_CURRENT);
+        MenuItem menuItemOne = mToolbar.getMenu().findItem(R.id.menu_item_rows_one);
+        menuItemOne.setChecked(rows == 1);
+        MenuItem menuItemTwo = mToolbar.getMenu().findItem(R.id.menu_item_rows_two);
+        menuItemTwo.setChecked(rows == 2);
+        MenuItem menuItemThree = mToolbar.getMenu().findItem(R.id.menu_item_rows_three);
+        menuItemThree.setChecked(rows == 3);
+        MenuItem menuItemFour = mToolbar.getMenu().findItem(R.id.menu_item_rows_four);
+        menuItemFour.setChecked(rows == 4);
+
+        int rowsLandscape = Settings.System.getIntForUser(
+                mContext.getContentResolver(), Settings.System.QS_LAYOUT_ROWS_LANDSCAPE, 2,
+                UserHandle.USER_CURRENT);
+        menuItemOne = mToolbar.getMenu().findItem(R.id.menu_item_rows_landscape_one);
+        menuItemOne.setChecked(rowsLandscape == 1);
+        menuItemTwo = mToolbar.getMenu().findItem(R.id.menu_item_rows_landscape_two);
+        menuItemTwo.setChecked(rowsLandscape == 2);
     }
 }