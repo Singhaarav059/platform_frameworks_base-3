BootleggersROM/frameworks_base

base: Add Lockscreen cover art filter
> Grayscale: to highlight the notifications colors
> Grayscale but accent tinted: To highlight accent colors
> Blur: An actual blur over the image
> Grayscale Blur: The previous blur but untinted to highligh notif colors
> Gradient Blur: Android Q gradient blur generated

Bootleggers edit: Updated to 10, and reworked to go along with the Blur intensity switch

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -59,11 +59,13 @@ import android.provider.Settings;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
 import com.android.internal.statusbar.NotificationVisibility;
+import com.android.internal.util.bootleggers.ImageHelper;
 import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.Interpolators;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.R;
 import com.android.systemui.statusbar.notification.NotificationEntryListener;
 import com.android.systemui.statusbar.notification.NotificationEntryManager;
 import com.android.systemui.statusbar.notification.collection.NotificationEntry;
@@ -155,6 +157,7 @@ public class NotificationMediaManager implements Dumpable {
 
     private boolean mShowCompactMediaSeekbar;
     private boolean mShowMediaMetadata;
+    private int mAlbumArtFilter;
     private final DeviceConfig.OnPropertiesChangedListener mPropertiesChangedListener =
             new DeviceConfig.OnPropertiesChangedListener() {
         @Override
@@ -255,6 +258,9 @@ public class NotificationMediaManager implements Dumpable {
             resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.LOCKSCREEN_MEDIA_METADATA),
                 false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.LOCKSCREEN_ALBUM_ART_FILTER),
+                false, this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -272,6 +278,9 @@ public class NotificationMediaManager implements Dumpable {
         mShowMediaMetadata = Settings.System.getIntForUser(resolver,
                 Settings.System.LOCKSCREEN_MEDIA_METADATA, 1,
                 UserHandle.USER_CURRENT) == 1;
+        mAlbumArtFilter = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_ALBUM_ART_FILTER, 5,
+                UserHandle.USER_CURRENT);
     }
 
     public static boolean isPlayingState(int state) {
@@ -763,7 +772,22 @@ public class NotificationMediaManager implements Dumpable {
     };
 
     private Bitmap processArtwork(Bitmap artwork) {
-        return mMediaArtworkProcessor.processArtwork(mContext, artwork, getLockScreenMediaBlurLevel());
+        switch (mAlbumArtFilter) {
+            case 0:
+                // TODO: Implement a proper fix to use this recycled bitmap
+                return Bitmap.createBitmap(ImageHelper.getBlurredImage(mContext, artwork, 0.001f));
+            case 1:
+                return Bitmap.createBitmap(ImageHelper.toGrayscale(artwork));
+            case 2:
+                return Bitmap.createBitmap(ImageHelper.getColoredBitmap(new BitmapDrawable(mBackdropBack.getResources(), artwork), mContext.getResources().getColor(R.color.system_light_accent)));
+            case 3:
+                return Bitmap.createBitmap(ImageHelper.getBlurredImage(mContext, artwork, getLockScreenMediaBlurLevel()));
+            case 4:
+                return Bitmap.createBitmap(ImageHelper.getGrayscaleBlurredImage(mContext, artwork, getLockScreenMediaBlurLevel()));
+            case 5:
+            default:
+                return mMediaArtworkProcessor.processArtwork(mContext, artwork, getLockScreenMediaBlurLevel());
+        }
     }
 
     @MainThread

Singhaarav059/platform_frameworks_base-3

packages/SystemUI/src/com/android/systemui/statusbar/NotificationMediaManager.java
@@ -58,11 +58,13 @@ import android.widget.ImageView;
 
 import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
 import com.android.internal.statusbar.NotificationVisibility;
+import com.android.internal.util.gzosp.ImageHelper;
 import com.android.systemui.Dependency;
 import com.android.systemui.Dumpable;
 import com.android.systemui.Interpolators;
 import com.android.systemui.colorextraction.SysuiColorExtractor;
 import com.android.systemui.plugins.statusbar.StatusBarStateController;
+import com.android.systemui.R;
 import com.android.systemui.statusbar.notification.NotificationEntryListener;
 import com.android.systemui.statusbar.notification.NotificationEntryManager;
 import com.android.systemui.statusbar.notification.collection.NotificationEntry;
@@ -153,6 +155,7 @@ public class NotificationMediaManager implements Dumpable {
 
     private boolean mShowCompactMediaSeekbar;
     private boolean mShowMediaMetadata;
+    private int mAlbumArtFilter;
     private final DeviceConfig.OnPropertiesChangedListener mPropertiesChangedListener =
             new DeviceConfig.OnPropertiesChangedListener() {
         @Override
@@ -253,6 +256,9 @@ public class NotificationMediaManager implements Dumpable {
             resolver.registerContentObserver(Settings.System.getUriFor(
                 Settings.System.LOCKSCREEN_MEDIA_METADATA),
                 false, this, UserHandle.USER_ALL);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                Settings.System.LOCKSCREEN_ALBUM_ART_FILTER),
+                false, this, UserHandle.USER_ALL);
             updateSettings();
         }
 
@@ -270,6 +276,9 @@ public class NotificationMediaManager implements Dumpable {
         mShowMediaMetadata = Settings.System.getIntForUser(resolver,
                 Settings.System.LOCKSCREEN_MEDIA_METADATA, 1,
                 UserHandle.USER_CURRENT) == 1;
+        mAlbumArtFilter = Settings.System.getIntForUser(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_ALBUM_ART_FILTER, 5,
+                UserHandle.USER_CURRENT);
     }
 
     public static boolean isPlayingState(int state) {
@@ -761,7 +770,22 @@ public class NotificationMediaManager implements Dumpable {
     };
 
     private Bitmap processArtwork(Bitmap artwork) {
-        return mMediaArtworkProcessor.processArtwork(mContext, artwork, getLockScreenMediaBlurLevel());
+        switch (mAlbumArtFilter) {
+            case 0:
+                // TODO: Implement a proper fix to use this recycled bitmap
+                return Bitmap.createBitmap(ImageHelper.getBlurredImage(mContext, artwork, 0.001f));
+            case 1:
+                return Bitmap.createBitmap(ImageHelper.toGrayscale(artwork));
+            case 2:
+                return Bitmap.createBitmap(ImageHelper.getColoredBitmap(new BitmapDrawable(mBackdropBack.getResources(), artwork), mContext.getResources().getColor(R.color.system_light_accent)));
+            case 3:
+                return Bitmap.createBitmap(ImageHelper.getBlurredImage(mContext, artwork, getLockScreenMediaBlurLevel()));
+            case 4:
+                return Bitmap.createBitmap(ImageHelper.getGrayscaleBlurredImage(mContext, artwork, getLockScreenMediaBlurLevel()));
+            case 5:
+            default:
+                return mMediaArtworkProcessor.processArtwork(mContext, artwork, getLockScreenMediaBlurLevel());
+        }
     }
 
     @MainThread