StagOS/android_frameworks_base_q

[1/2] base: OmniSwitch integration
do it on a lower level this time so we catch all
calls in one place

Change-Id: I53d83bb8e288f78bec558086ae33cfedb19c1e71
Signed-off-by: Ravi Pingat <pingatravi@gmail.com>

core/java/com/android/internal/util/stag/PackageUtils.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2014-2018 The OmniROM Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.stag;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class PackageUtils {
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -19,7 +19,11 @@ package com.android.systemui.recents;
 import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.graphics.Rect;
+import android.os.UserHandle;
 import android.provider.Settings;
+
+import com.android.systemui.Dependency;
+import com.android.internal.util.stag.StagUtils;
 
 import com.android.systemui.R;
 import com.android.systemui.SystemUI;
@@ -72,6 +76,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            StagUtils.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         mImpl.showRecentApps(triggeredFromAltTab);
     }
 
@@ -80,6 +89,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            StagUtils.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -94,6 +108,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            StagUtils.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         mImpl.toggleRecentApps();
     }
 
@@ -102,6 +121,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            StagUtils.preloadOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -115,7 +139,9 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
         if (!isUserSetup()) {
             return;
         }
-
+        if (isOmniSwitchRecents()) {
+            return;
+        }
         mImpl.cancelPreloadRecentApps();
     }
 
@@ -164,5 +190,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
     @Override
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         mImpl.dump(pw);
+    }
+
+    private boolean isOmniSwitchRecents() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_NAVIGATION_BAR_RECENTS, 0,
+                    UserHandle.USER_CURRENT) == 1;
     }
 }

Singhaarav059/platform_frameworks_base-3

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/java/com/android/internal/util/colt/PackageUtils.java
@@ -1,0 +1,46 @@
+/*
+* Copyright (C) 2014-2018 The OmniROM Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.colt;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+
+public class PackageUtils {
+
+    public static boolean isAppInstalled(Context context, String appUri) {
+        try {
+            PackageManager pm = context.getPackageManager();
+            pm.getPackageInfo(appUri, PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isAvailableApp(String packageName, Context context) {
+        Context mContext = context;
+        final PackageManager pm = mContext.getPackageManager();
+        try {
+            pm.getPackageInfo(packageName, PackageManager.GET_ACTIVITIES);
+            int enabled = pm.getApplicationEnabledSetting(packageName);
+            return enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED &&
+                enabled != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER;
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+    }
+}

packages/SystemUI/src/com/android/systemui/recents/Recents.java
@@ -19,7 +19,11 @@ package com.android.systemui.recents;
 import android.content.ContentResolver;
 import android.content.res.Configuration;
 import android.graphics.Rect;
+import android.os.UserHandle;
 import android.provider.Settings;
+
+import com.android.systemui.Dependency;
+import com.android.internal.util.colt.ColtUtils;
 
 import com.android.systemui.R;
 import com.android.systemui.SystemUI;
@@ -72,6 +76,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            ColtUtils.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         mImpl.showRecentApps(triggeredFromAltTab);
     }
 
@@ -80,6 +89,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            ColtUtils.hideOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -94,6 +108,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
             return;
         }
 
+        if (isOmniSwitchRecents()) {
+            ColtUtils.toggleOmniSwitchRecents(mContext, UserHandle.CURRENT);
+            return;
+        }
+
         mImpl.toggleRecentApps();
     }
 
@@ -102,6 +121,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
         // Ensure the device has been provisioned before allowing the user to interact with
         // recents
         if (!isUserSetup()) {
+            return;
+        }
+
+        if (isOmniSwitchRecents()) {
+            ColtUtils.preloadOmniSwitchRecents(mContext, UserHandle.CURRENT);
             return;
         }
 
@@ -115,7 +139,9 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
         if (!isUserSetup()) {
             return;
         }
-
+        if (isOmniSwitchRecents()) {
+            return;
+        }
         mImpl.cancelPreloadRecentApps();
     }
 
@@ -164,5 +190,11 @@ public class Recents extends SystemUI implements CommandQueue.Callbacks {
     @Override
     public void dump(FileDescriptor fd, PrintWriter pw, String[] args) {
         mImpl.dump(pw);
+    }
+
+    private boolean isOmniSwitchRecents() {
+        return Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.OMNI_NAVIGATION_BAR_RECENTS, 0,
+                    UserHandle.USER_CURRENT) == 1;
     }
 }