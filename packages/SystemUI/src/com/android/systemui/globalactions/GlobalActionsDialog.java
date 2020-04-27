885d6c220c91e33b0d4c267340f1262e842d6b8d
Advaith Bhat <advaithbhat9@gmail.com>
2020-04-27 10:44:59 +0000

    base: Blur behind powermenu
    * Idea taken from Decendents
    * Thanks to SuperDroidBond For helping
    
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -38,6 +38,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.UserInfo;
 import android.database.ContentObserver;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraCharacteristics;
@@ -112,6 +114,8 @@ import com.android.systemui.volume.SystemUIInterpolators.LogAccelerateInterpolat
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.android.systemui.colt.ImageUtils;
 
 /**
  * Helper to show the global actions dialog.  Each item is an {@link Action} that
@@ -1826,7 +1830,10 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
 
             // Window initialization
             Window window = getWindow();
+            View v1 = window.getDecorView();
             window.requestFeature(Window.FEATURE_NO_TITLE);
+            Bitmap bittemp = ImageUtils.blurImage(mContext, ImageUtils.screenshotSurface(mContext));
+            Drawable background = new BitmapDrawable(mContext.getResources(), bittemp);
             // Inflate the decor view, so the attributes below are not overwritten by the theme.
             window.getDecorView();
             window.getAttributes().systemUiVisibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -1895,7 +1902,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                                 FrameLayout.LayoutParams.MATCH_PARENT,
                                 FrameLayout.LayoutParams.MATCH_PARENT);
                 panelContainer.addView(mPanelController.getPanelContent(), panelParams);
-                mBackgroundDrawable = mPanelController.getBackgroundDrawable();
+               //  mBackgroundDrawable = mPanelController.getBackgroundDrawable();
                 mScrimAlpha = 1f;
             }
         }
@@ -1926,6 +1933,9 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                 mScrimAlpha = ScrimController.GRADIENT_SCRIM_ALPHA;
             }
             getWindow().setBackgroundDrawable(mBackgroundDrawable);
+            Bitmap bittemp = ImageUtils.blurImage(mContext, ImageUtils.screenshotSurface(mContext));
+            Drawable background = new BitmapDrawable(mContext.getResources(), bittemp);
+            getWindow().setBackgroundDrawable(background);
         }
 
         private void fixNavBarClipping() {
@@ -2004,10 +2014,10 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mShowing = true;
             mHadTopUi = mStatusBarWindowController.getForceHasTopUi();
             mStatusBarWindowController.setForceHasTopUi(true);
-            mBackgroundDrawable.setAlpha(0);
+            // mBackgroundDrawable.setAlpha(0);
             mGlobalActionsLayout.setTranslationX(mGlobalActionsLayout.getAnimationOffsetX());
             mGlobalActionsLayout.setTranslationY(mGlobalActionsLayout.getAnimationOffsetY());
-            mGlobalActionsLayout.setAlpha(0);
+            // mGlobalActionsLayout.setAlpha(0);
             mGlobalActionsLayout.animate()
                     .alpha(1)
                     .translationX(0)
@@ -2017,7 +2027,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((Float) animation.getAnimatedValue()
                                 * mScrimAlpha * 255);
-                        mBackgroundDrawable.setAlpha(alpha);
+                        // mBackgroundDrawable.setAlpha(alpha);
                     })
                     .start();
         }
@@ -2041,7 +2051,7 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
                     .setUpdateListener(animation -> {
                         int alpha = (int) ((1f - (Float) animation.getAnimatedValue())
                                 * mScrimAlpha * 255);
-                        mBackgroundDrawable.setAlpha(alpha);
+                        // mBackgroundDrawable.setAlpha(alpha);
                     })
                     .start();
             dismissPanel();
