522e6bfdbf571ad4cdddfa9da1485010cfb0ae44
SuperDroidBond <superdroidbond@yahoo.com>
2020-05-21 01:08:08 +0530

    Random Qs Tint: tint active QS Tiles with random colors [1/2]
    Xtended the QS Tint Options to -:
      1) 1 = Random Color Tint (Default)
      2) 2 = Accent Based Tint
      3) 3 = Off (Our Beloved Gradient Shit)
    
    Special Thanks to Potato ROM for idea -
        https://github.com/PotatoProject/frameworks_base/commit/8b30ef966ddb8f28763998e3aabf69e62b7351a4
    
    Change-Id: I388c68c69bc5da791e617dc87e46c077805a9052
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileBaseView.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileBaseView.java
@@ -19,6 +19,7 @@ import android.annotation.ColorInt;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.ColorStateList;
+import android.content.res.ColorUtils;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Path;
@@ -74,6 +75,7 @@ public class QSTileBaseView extends com.android.systemui.plugins.qs.QSTileView {
     private final int mColorDisabled;
     private int mCircleColor;
     private int mBgSize;
+    private int setQsUseNewTint;
 
     public QSTileBaseView(Context context, QSIconView icon) {
         this(context, icon, false);
@@ -122,16 +124,14 @@ public class QSTileBaseView extends com.android.systemui.plugins.qs.QSTileView {
 
         mColorActive = Utils.getColorAttrDefaultColor(context, android.R.attr.colorAccent);
         mColorActiveAlpha = adjustAlpha(mColorActive, 0.2f);
-        boolean setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
-                    Settings.System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT) == 1;
-        if (setQsUseNewTint) {
+        setQsUseNewTint = Settings.System.getIntForUser(context.getContentResolver(),
+                    Settings.System.QS_PANEL_BG_USE_NEW_TINT, 0, UserHandle.USER_CURRENT);
+        if (setQsUseNewTint != 0) {
             mColorActive = mColorActiveAlpha;
-            mColorDisabled = context.getResources().getColor(R.color.qs_tile_background_color_disabled);
-        } else {
-            mColorDisabled = Utils.getDisabled(context,
-                    Utils.getColorAttrDefaultColor(context, android.R.attr.textColorTertiary));
         }
         mColorInactive = Utils.getColorAttrDefaultColor(context, android.R.attr.textColorSecondary);
+        mColorDisabled = Utils.getDisabled(context,
+                Utils.getColorAttrDefaultColor(context, android.R.attr.textColorTertiary));
 
         setPadding(0, 0, 0, 0);
         setClipChildren(false);
