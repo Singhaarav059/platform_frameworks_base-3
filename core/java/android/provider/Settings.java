AOSPA/android_frameworks_base-old

[1/2] SystemUI: Introduce Adaptive Playback
Automatically pause media when muted. Resume if unmuted within a minute.

Change-Id: I5d37478b3739309dcaae4eb6a0e4aac6c87f120f
Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>

core/java/android/provider/Settings.java
@@ -4660,6 +4660,16 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         *  Adaptive playback
+         *  Automatically pause media when the volume is muted and
+         *  will resume automatically when volume is restored.
+         *    0 = disabled
+         *    1 = enabled
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK = "adaptive_playback";
+
+        /**
          * Whether allowing pocket service to register sensors and dispatch informations.
          *   0 = disabled
          *   1 = enabled

Singhaarav059/platform_frameworks_base-3

Co-Authored-By: Stylogey <stylogey@gmail.com>
Signed-off-by: Joey Huab <joey@evolution-x.org>
Signed-off-by: ashwatthama <sai404142@gmail.com>
Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

core/java/android/provider/Settings.java
@@ -6407,6 +6407,22 @@ public final class Settings {
         private static final Validator BATTERY_LIGHT_REALLYFULL_COLOR_VALIDATOR =
                 ANY_STRING_VALIDATOR;
 
+	/**
+         * Adaptive playback
+         * Automatically pause media when the volume is muted and
+         * will resume automatically when volume is restored.
+         *   0 = disabled
+         *   1 = enabled
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_ENABLED = "adaptive_playback_enabled";
+
+        /**
+         * Adaptive playback's timeout in ms
+         * @hide
+         */
+        public static final String ADAPTIVE_PLAYBACK_TIMEOUT = "adaptive_playback_timeout";
+
         /**
          * Header height
          * @hide