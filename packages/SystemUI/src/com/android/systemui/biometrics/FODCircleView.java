crdroidandroid/android_frameworks_base

FODCircleView: add FOD invisible icon [1/2]

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -121,7 +121,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         R.drawable.fod_icon_shooky,
         R.drawable.fod_icon_spiral_blue,
         R.drawable.fod_icon_sun_metro,
-        R.drawable.fod_icon_default_1
+        R.drawable.fod_icon_default_1,
+        R.drawable.fod_icon_invisible
     };
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =

Singhaarav059/platform_frameworks_base-3

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -127,7 +127,8 @@ public class FODCircleView extends ImageView implements ConfigurationListener {
         R.drawable.fod_icon_rainbow_horn,
         R.drawable.fod_icon_shooky,
         R.drawable.fod_icon_spiral_blue,
-        R.drawable.fod_icon_sun_metro
+        R.drawable.fod_icon_sun_metro,
+        R.drawable.fod_icon_invisible
     };
 
     private IFingerprintInscreenCallback mFingerprintInscreenCallback =