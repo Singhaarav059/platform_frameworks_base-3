60c39bd815df97239daa8518381a2df9b84dcce9
Alex Cruz <du.alexcruz@gmail.com>
2020-06-10 16:47:36 -0400

    Lockscreen timeout: Make timeout configurable [1/2]
    Change-Id: Id95e90ef068eacaeb0a609865aa9f0f63f7390a0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6620,6 +6620,13 @@ public final class Settings {
         public static final String BACK_GESTURE_HAPTIC = "back_gesture_haptic";
 
         /**
+         * The amount of time in milliseconds before the device goes to sleep or begins
+         * to dream after a period of inactivity in the keyguard.
+         * @hide
+         */
+        public static final String LOCKSCREEN_TIMEOUT = "lockscreen_timeout";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
