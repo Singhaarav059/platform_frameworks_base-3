0407bc5d8f22e548012b10850a82051fd18f7a3b
RakeshBatra <raakesh.batra@rediffmail.com>
2020-07-05 10:38:09 -0700

    Add missing import
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/google/android/systemui/keyguard/KeyguardSliceProviderGoogle.java
+++ packages/SystemUI/src/com/google/android/systemui/keyguard/KeyguardSliceProviderGoogle.java
@@ -14,6 +14,7 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.os.Trace;
+import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
 import androidx.core.graphics.drawable.IconCompat;
