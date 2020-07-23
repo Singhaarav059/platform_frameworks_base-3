777931b86e0eee0bff32ae61d5fa79b33a159d41
spezi77 <spezi7713@gmx.net>
2020-07-24 01:17:34 +0200

    SystemUI: Add Space Analog Clock Style. * Clock illustration by the master himself @ROGERdotT adapted to ColtOS
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I3b6f8071afde04b41c8d18402c9c29b4ec6dacfe
    Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -150,6 +150,7 @@ public final class ClockManager {
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new DividedLinesClockController(res, layoutInflater, colorExtractor));
 	addBuiltinClock(() -> new ColtClockController(res, layoutInflater, colorExtractor));
+	addBuiltinClock(() -> new SpaceClockController(res, layoutInflater, colorExtractor, context));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
